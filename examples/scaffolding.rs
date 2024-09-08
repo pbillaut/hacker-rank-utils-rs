38d7d8226a4ee16bff384927a0ba2ee01549a425
Patrice Billaut <pbillaut@pm.me>
2024-09-08 16:52:24 +0200

    add example scaffolding

---
+++ examples/scaffolding.rs
@@ -0,0 +1,68 @@
+use std::{error::Error, fmt::Debug, io, io::{BufRead, Lines}, str::FromStr};
+
+fn solution() {
+    // Enter your code here
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut reader = InputReader::new(stdin.lock());
+    // Enter your parsing code here
+    solution();
+}
+
+pub type Result<T> = core::result::Result<T, Box<dyn Error>>;
+
+pub struct InputReader<R>
+where
+    R: BufRead,
+{
+    lines: Lines<R>,
+}
+
+impl<R> InputReader<R>
+where
+    R: BufRead,
+{
+    pub fn new(reader: R) -> Self {
+        Self { lines: reader.lines() }
+    }
+
+    pub fn lines(&mut self) -> &mut Lines<R> {
+        &mut self.lines
+    }
+
+    pub fn next_value<T>(&mut self) -> hacker_rank_utils_rs::Result<T>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        self.lines
+            .next()
+            .ok_or("Unable to parse next value: iterator is empty")?
+            .map(|line| line.trim().parse())?
+            .map_err(|err| format!("Unable to parse next value: {:?}", err).into())
+    }
+
+    pub fn next_values<T>(&mut self, num_values: usize) -> hacker_rank_utils_rs::Result<Vec<T>>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        let mut list: Vec<T> = Vec::with_capacity(num_values);
+        for _ in 0..num_values {
+            let value = self.next_value()?;
+            list.push(value);
+        }
+        Ok(list)
+    }
+
+    pub fn next_vector<T>(&mut self) -> hacker_rank_utils_rs::Result<Vec<T>>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        let num_values = self.next_value::<usize>()?;
+        self.next_values(num_values)
+    }
+}
